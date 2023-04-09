40b554faf947037d42bb69b2205fbb0ac4f42532
William Pong <williampongcoding@gmail.com>
2023-04-09 17:25:17 +0800

    Add Stop Loss

--- sma.py
+++ sma.py
@@ -7,12 +7,23 @@ class SMAAnalyzer:
     """
     A class for analyzing stock data using simple moving averages (SMA).
     """
-    def __init__(self, ticker, start='2015-01-01', end="2022-01-01", sma_short=50, sma_long=200):
+    def __init__(self, ticker, start='2015-01-01', end="2022-01-01", sma_short=50, sma_long=200, stop_loss=True, stop_loss_percent=0.08):
+        """
+        ticker: stock code
+        start: start day
+        end: end day
+        sma_short: eg.SMA50
+        sma_long: eg.SMA200
+        stop_loss: Set True if having a stop_loss_percent
+        stop_loss_percent: loss percentage compared to the buying price
+        """
         self.ticker = ticker
         self.start = start
         self.end = end
         self.sma_short = sma_short
         self.sma_long = sma_long
+        self.stop_loss = stop_loss
+        self.stop_loss_percent = stop_loss_percent
 
     def download_data(self):
         """
@@ -24,31 +35,46 @@ class SMAAnalyzer:
         """
         Buy and Sell signal
         """
-        signal_Buy = []
-        signal_Sell = []
+        buy_signal = []
+        sell_signal = []
+        stop_loss_signal = []
         flag = -1
+        stop_loss_price = 0
+        stop_loss = self.stop_loss
+        stop_loss_percent = self.stop_loss_percent
         for i in range(len(data)):
-            if(data[f'SMA{self.sma_short}'][i] > data[f'SMA{self.sma_long}'][i]):
-                if(flag != 1):
-                    signal_Buy.append(data['Ticker'][i])
-                    signal_Sell.append(np.nan)
+            if data[f'SMA{self.sma_short}'][i] > data[f'SMA{self.sma_long}'][i] and data['Ticker'][i] > stop_loss_price:
+                stop_loss_signal.append(np.nan)
+                if flag == 0:
+                    buy_signal.append(data['Ticker'][i])
+                    if stop_loss:
+                        stop_loss_price = data['Ticker'][i] * (1 - stop_loss_percent)
+                    sell_signal.append(np.nan)
                     flag = 1
                 else:
-                    signal_Buy.append(np.nan)
-                    signal_Sell.append(np.nan)
-            elif(data[f'SMA{self.sma_short}'][i] < data[f'SMA{self.sma_long}'][i]):
-                if(flag != -1 and flag != 0):
-                    signal_Buy.append(np.nan)
-                    signal_Sell.append(data['Ticker'][i])
-                    flag = 0
+                    buy_signal.append(np.nan)
+                    sell_signal.append(np.nan)
+            elif data[f'SMA{self.sma_short}'][i] < data[f'SMA{self.sma_long}'][i] or data['Ticker'][i] < stop_loss_price:
+                if flag == 1:
+                    buy_signal.append(np.nan)
+                    sell_signal.append(data['Ticker'][i])
+                    flag = -1
+                    if data['Ticker'][i] < stop_loss_price:
+                        stop_loss_signal.append(data['Ticker'][i])
+                    else:
+                        stop_loss_signal.append(np.nan)
                 else:
-                    signal_Buy.append(np.nan)
-                    signal_Sell.append(np.nan)
+                    buy_signal.append(np.nan)
+                    sell_signal.append(np.nan)
+                    stop_loss_signal.append(np.nan)
+                if data[f'SMA{self.sma_short}'][i] < data[f'SMA{self.sma_long}'][i]:
+                    flag = 0
             else:
-                signal_Buy.append(np.nan)
-                signal_Sell.append(np.nan)
+                buy_signal.append(np.nan)
+                sell_signal.append(np.nan)
+                stop_loss_signal.append(np.nan)
         self.data = data
-        return signal_Buy, signal_Sell
+        return buy_signal, sell_signal, stop_loss_signal
 
     def plot_data(self):
         """
@@ -62,6 +88,7 @@ class SMAAnalyzer:
         plt.plot(data[f'SMA{self.sma_long}'], label=f'SMA{self.sma_long}', alpha=0.45)
         plt.scatter(data.index, data['Buy_Signal_Price'], label='Buy', marker='^', color='green')
         plt.scatter(data.index, data['Sell_Signal_Price'], label='Sell', marker='^', color='red')
+        plt.scatter(data.index, data['Stop_Loss'], label='Stop Loss', marker='x', color='red', linewidth=3)
         plt.legend(loc='upper left')
         plt.title(f'{self.ticker.upper()} Adj Close Price & Signals')
         plt.xlabel('DateTime')
@@ -75,18 +102,21 @@ class SMAAnalyzer:
         data = self.data
         buy = data['Buy_Signal_Price'].dropna().reset_index(drop=False)
         sell = data['Sell_Signal_Price'].dropna().reset_index(drop=False)
+        stop_loss = data['Stop_Loss'].dropna().reset_index(drop=False)
         merged_df = buy.join(sell, lsuffix='_Buy', rsuffix='_Sell')
         merged_df = merged_df[['Date_Buy', 'Buy_Signal_Price', 'Date_Sell', 'Sell_Signal_Price']].dropna()
         merged_df['Holding Days'] = merged_df['Date_Sell'] - merged_df['Date_Buy']
         merged_df['Profit'] = merged_df['Sell_Signal_Price'] - merged_df['Buy_Signal_Price']
         merged_df['Win'] = (merged_df['Profit'] > 0).astype(int)
         merged_df['Lose'] = (merged_df['Profit'] <= 0).astype(int)
-        
+        merged_df['Stop Loss'] = merged_df['Date_Sell'].isin(stop_loss['Date']).astype(int)
+
         num_trades = len(merged_df)
         profitable_trades = merged_df['Win'].sum()
         average_holding_days = merged_df['Holding Days'].mean().days
         losing_trades = merged_df['Lose'].sum()
         total_profit = merged_df['Profit'].sum() 
+        stop_loss = merged_df['Stop Loss'].sum()
         
         if num_trades > 0:
             win_probability = f'{round(profitable_trades / num_trades * 100, 2)} %'
@@ -99,8 +129,9 @@ class SMAAnalyzer:
         print(f"Average holding period: {average_holding_days} days")
         print(f"Number of profitable trades: {profitable_trades}")
         print(f"Number of losing trades: {losing_trades}")
+        print(f"Number of stop losses: {stop_loss}")
         print(f"Total profit: {total_profit:.2f}")
-        print(f"Win Probability: {win_probability}")     
+        print(f"Win rate: {win_probability}")     
 
     def show_performance(self):
         df = self.download_data()
@@ -109,7 +140,7 @@ class SMAAnalyzer:
         data['Ticker'] = df['Adj Close']
         data[f'SMA{self.sma_short}'] = df['Adj Close'].rolling(window=self.sma_short).mean()
         data[f'SMA{self.sma_long}'] = df['Adj Close'].rolling(window=self.sma_long).mean()
-        data['Buy_Signal_Price'], data['Sell_Signal_Price']  = self.buy_sell(data)
+        data['Buy_Signal_Price'], data['Sell_Signal_Price'], data['Stop_Loss']  = self.buy_sell(data)
 
         self.analyze_performance()
         self.plot_data()
